88cdfed9f5ad3ce219c772472fed13f2f6f5401e
pr1zrox <pr1zrox@gmail.com>
2013-11-22 03:28:36 +0400

    small fixes

--- lab3/main.rb
+++ lab3/main.rb
@@ -8,41 +8,51 @@ BUFFER_SIZE = 32 * 1024
 case type
 
 when 'server'
-    puts 'lol sirvachok'
 
-    Socket.open(AF_INET,SOCK_STREAM,0) { |s|
-      s.setsockopt(Socket::SOL_SOCKET,Socket::SO_REUSEADDR,true)
+    Socket.open(AF_INET,SOCK_STREAM,0) do |s|
+      s.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
 
-      s.bind(Socket.sockaddr_in(port_num,''))
-      s.listen(5)
+      s.bind(Socket.sockaddr_in(port_num, ''))
+      s.listen(1)
+
+      puts 'Server is listening'
+
       client_socket_fd, _ = s.sysaccept
 
       client_socket = Socket.for_fd(client_socket_fd)
 
-      open(file_address, 'r:binary') { |f|
-        while read_data = f.read(BUFFER_SIZE)
+      open(file_address, 'r:binary') do |f|
+        while (read_data = f.read(BUFFER_SIZE))
           client_socket.write(read_data)
         end
-      }
-    }
+
+        client_socket.close
+      end
+
+      puts 'File transfer finished'
+    end
 
 when  'client'
-    puts 'lol clientik'
 
-    Socket.open(AF_INET,SOCK_STREAM,0) { |s|
-      s.setsockopt(Socket::SOL_SOCKET,Socket::SO_REUSEADDR,true)
-      s.connect(Socket.sockaddr_in(port_num,host_adr))
+    Socket.open(AF_INET,SOCK_STREAM,0) do |s|
+      s.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+      s.connect(Socket.sockaddr_in(port_num, host_adr))
 
-      open(file_address + '_copy', 'w:binary') { |f|
-        while received_data = s.read
+      puts 'Client connected'
+
+      open(file_address + '_copy', 'w:binary') do |f|
+        while (received_data = s.read)
+          if received_data == ''
+            puts 'File transfer finished'
+            break
+          end
           f.write(received_data)
         end
-      }
-
-    }
+      end
+    end
 
 else
-    puts '  dunno lol'
+    puts 'i dunno lol'
 end
 
 
