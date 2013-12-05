7e98bfaf6b2237479108228409ccfd7c5af80eb3
pr1zrox <pr1zrox@gmail.com>
2013-12-06 01:51:51 +0400

    add socket info comments

--- lab3/main.rb
+++ lab3/main.rb
@@ -2,71 +2,75 @@ require 'socket'
 
 include Socket::Constants
 
-type, file_address, port_num, host_adr  = ARGV
+type, file_address, port_num, host_adr = ARGV
 BUFFER_SIZE = 32 * 1024
 
 case type
 
 when 'server'
 
-    Socket.open(AF_INET,SOCK_STREAM,0) do |s|
-      s.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+  Socket.open(AF_INET, SOCK_STREAM, 0) do |s|
+    s.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
 
-      s.bind(Socket.sockaddr_in(port_num, ''))
-      s.listen(1)
+    #AF_INET - ipv4 address family
+    #SOCK_STREAM - TCP
+    #SOL_SOCKET - socket option level
 
-      puts 'Server is listening'
+    s.bind(Socket.sockaddr_in(port_num, ''))
+    s.listen(1)
 
-      client_socket_fd, _ = s.sysaccept
+    puts 'Server is listening'
 
-      client_socket = Socket.for_fd(client_socket_fd)
+    client_socket_fd, _ = s.sysaccept
 
-      open(file_address, 'r:binary') do |f|
-        while (read_data = f.read(BUFFER_SIZE))
-          begin
-            client_socket.write(read_data)
-          rescue Errno::ECONNRESET
-            puts 'Client disconnected'
-            break
-          rescue Errno::EPIPE
-            puts 'Client disconnected'
-            break
-          end
+    client_socket = Socket.for_fd(client_socket_fd)
 
-        end
-
-        client_socket.close
-      end
-
-      puts 'Server finished work'
-    end
-
-when  'client'
-
-    Socket.open(AF_INET,SOCK_STREAM,0) do |s|
-      s.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
-      s.connect(Socket.sockaddr_in(port_num, host_adr))
-
-      puts 'Client connected'
-
-      open(file_address + '_copy', 'w:binary') do |f|
+    open(file_address, 'r:binary') do |f|
+      while (read_data = f.read(BUFFER_SIZE))
         begin
-          while (received_data = s.read)
-            if received_data == ''
-              puts 'File transfer finished'
-              break
-            end
-            f.write(received_data)
-          end
+          client_socket.write(read_data)
+        rescue Errno::ECONNRESET
+          puts 'Client disconnected'
+          break
         rescue Errno::EPIPE
-          puts 'Server disconnected'
+          puts 'Client disconnected'
           break
         end
+
       end
+
+      client_socket.close
     end
 
+    puts 'Server finished work'
+  end
+
+when 'client'
+
+  Socket.open(AF_INET, SOCK_STREAM, 0) do |s|
+    s.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+    s.connect(Socket.sockaddr_in(port_num, host_adr))
+
+    puts 'Client connected'
+
+    open(file_address + '_copy', 'w:binary') do |f|
+      begin
+        while (received_data = s.read)
+          if received_data == ''
+            puts 'File transfer finished'
+            break
+          end
+          f.write(received_data)
+        end
+      rescue Errno::EPIPE
+        puts 'Server disconnected'
+        break
+      end
+    end
+  end
+
 else
-    puts 'i dunno lol'
+  puts 'i dunno lol'
 end
 
 
